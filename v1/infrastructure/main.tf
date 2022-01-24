d9eb6ab638cfda10b1b423317126bcecafe7c0e3
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-24 21:21:35 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -14,3 +14,11 @@ provider "aws" {
   version = "~>3.0"
   region  = "eu-west-1"
 }
+
+required_version = ">=0.14.9" 
+
+   backend "s3" {
+       bucket = "[Remote_State_S3_Bucket_Name]"
+       key    = "[Remote_State_S3_Bucket_Key]"
+       region = "east-us-1"
+   }

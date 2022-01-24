cda1dc3da0b229f5a8560574ea78e3785ebf1fd5
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-24 20:44:56 +0000

    Create terraform providers

---
+++ v1/infrastructure/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "eu-west-1"
+}

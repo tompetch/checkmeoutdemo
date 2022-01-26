222cc10043a487b41ac5a2866d7ca80e64aa709f
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-26 22:13:11 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_object" "index_html" {
   bucket = "checkmeoutdemo.tompetch.com"
   key    = "index.html"
   acl    = "private"  # or can be "public-read"
-  source = "v1/application/index.html"
+  source = "./v1/application/index.html"
   etag = filemd5("v1/application/index.html")
 
 }

20816a19cffc293240e0e128fccd7724d7ee36ee
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-26 21:41:15 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -14,9 +14,23 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "checkmeoutdemo.tompetch.com"
   acl    = "public-read"
+    
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   website {
     index_document = "index.html"

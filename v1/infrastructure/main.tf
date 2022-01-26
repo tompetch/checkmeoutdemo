62d4c40c696cdb8ecbc598b1308107ab5ec1db0f
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-26 20:29:56 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -1,24 +1,11 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
+  backend "s3" {
+    # Bucket name
+    bucket         = "checkmeoutdemoterraformstate"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-1"
+    # DynamoDB table name
+    dynamodb_table = "checkmeoutdemoterraformstatelock"
+    encrypt        = true
   }
-
-  required_version = ">=0.14.9"
-
 }
-
-provider "aws" {
-  version = "~>3.0"
-  region  = "eu-west-1"
-}
-
-required_version = ">=0.14.9" 
-
-   backend "s3" {
-       bucket = "[Remote_State_S3_Bucket_Name_v1]"
-       key    = "[Remote_State_S3_Bucket_Key_v1]"
-       region = "eu-west-1"
-   }

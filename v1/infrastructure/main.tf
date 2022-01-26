27fb5fca7418f1ad461791885a175f5df5799e55
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-26 21:08:18 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -9,3 +9,25 @@ terraform {
     encrypt        = true
   }
 }
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "checkmeoutdemo.tompetch.com"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}

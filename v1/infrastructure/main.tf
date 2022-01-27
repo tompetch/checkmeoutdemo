fe7cef37348978f9e54decaa4c3ce8f9a7a70d73
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-27 17:19:48 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -134,3 +134,23 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     ssl_support_method  = "sni-only"
   }
 }
+
+// We want AWS to host our zone so its nameservers can point to our CloudFront
+// distribution.
+resource "aws_route53_zone" "zone" {
+  name = "${var.domain_name}"
+}
+
+// This Route53 record will point at our CloudFront distribution.
+resource "aws_route53_record" "route53_record" {
+  zone_id = "${aws_route53_zone.zone.zone_id}"
+  name    = "${var.domain_name}"
+  type    = "A"
+
+  alias = {
+    name                   = "${aws_cloudfront_distribution.website_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.website_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+

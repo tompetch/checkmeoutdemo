326a48de1e56a3710945bb5d526d87d44116e06e
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-27 14:19:43 +0000

    update cert

--- v1/infrastructure/variables.tf
+++ v1/infrastructure/variables.tf
@@ -4,3 +4,6 @@ variable "bucketname" {
 variable "domain_name" {
   default = "www.checkmeout-demo.com"
 }
+variable "cert_arn" {
+  default = "arn:aws:acm:us-east-1:732372734688:certificate/e9357391-60f0-4c38-9acc-f547a8eded93"
+}

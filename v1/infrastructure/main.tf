2355c3f9106c402e4d381f2972bc3cdee7bd6f37
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-27 09:33:23 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -14,10 +14,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucketname
@@ -26,8 +22,6 @@ resource "aws_s3_bucket" "website_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
       }
     }
   }

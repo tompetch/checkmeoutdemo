2555848420b0b699f8fb979f75cd2f0510b5aa5a
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-27 13:15:40 +0000

    create cert

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -64,3 +64,13 @@ EOF
 EOF
   }
 }
+
+resource "aws_acm_certificate" "certificate" {
+  // We want a wildcard cert so we can host subdomains later.
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "EMAIL"
+
+  // We also want the cert to be valid for the root domain even though we'll be
+  // redirecting to the www. domain immediately.
+  subject_alternative_names = ["${var.domain_name}"]
+}
--- v1/infrastructure/variables.tf
+++ v1/infrastructure/variables.tf
@@ -1,3 +1,7 @@
 variable "bucketname" {
     default = "checkmeoutdemo.tompetch.com"
 }
+
+variable "www_domain_name" {
+  default = "www.checkmeout-demo.com"
+}

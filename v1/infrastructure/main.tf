06d4069ea4eb0b5465364001727debb3187de196
tompetch <70695082+tompetch@users.noreply.github.com>
2022-01-27 08:33:46 +0000

    Update main.tf

--- v1/infrastructure/main.tf
+++ v1/infrastructure/main.tf
@@ -31,6 +31,25 @@ resource "aws_s3_bucket" "website_bucket" {
       }
     }
   }
+policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::checkmeoutdemo.tompetch.com"
+            ]
+        }
+    ]
+}
+EOF
+  
 
   website {
     index_document = "index.html"
